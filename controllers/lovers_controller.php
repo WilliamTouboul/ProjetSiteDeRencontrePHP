765353693497eb1b7745d2a228b81d85b83bbb60
nourhene <nourhene.fredj@gmail.com>
2021-01-27 14:10:18 +0100

    galere de la navbar

--- controllers/lovers_controller.php
+++ controllers/lovers_controller.php
@@ -2,54 +2,204 @@
 <?php
 $TabLovers = [
     [
-        'lastname' => 'Mathilde',
-        'firstname' => 'pierrau',
+        'lastname' => 'Pierrau',
+        'firstname' =>'Mathilde',
         'age' => 29,
-        'gender' => 'femme',
-        'mail' => 'mathilde.pierrau@gmail.com',
+        'gender' => 'Femme',
         'zipcode' => '93110',
         'picture' => '\assets\img\mathilde.jpg',
         'description' => 'jeune femme romatique ........'
     ],
     [
-        'lastname' => 'Jerome',
-        'firstname' => 'Coincé',
+        'lastname' => 'Lecoincé',
+        'firstname' => 'Jerome',
         'age' => 35,
-        'gender' => 'homme',
-        'mail' => 'jeromelecoince@gmail.com',
+        'gender' => 'Homme',
         'zipcode' => '75000',
         'picture' => '\assets\img\jerome.jpg',
         'description' => 'jeune homme un peu timide ........'
     ],
     [
-        'lastname' => 'Jerome',
-        'firstname' => 'Coincé',
-        'age' => 35,
-        'gender' => 'homme',
-        'mail' => 'jeromelecoince@gmail.com',
-        'zipcode' => '75000',
+        'lastname' => 'Nourhene',
+        'firstname' =>'Fredj' ,
+        'age' => 32,
+        'gender' => 'Femme',
+        'zipcode' => '76460',
+        'picture' => '\assets\img\mathilde.jpg',
+        'description' => 'femmezetydujftgrtcfdtcbtvhj ........'
+    ],
+    [
+        'lastname' => 'Touboul',
+        'firstname' => 'William',
+        'age' => 25,
+        'gender' => 'Homme',
+        'zipcode' => '76000',
         'picture' => '\assets\img\jerome.jpg',
-        'description' => 'jeune homme un peu timide ........'
+        'description' => 'jeune homme zseazzaazeeza ........'
     ],
     [
-        'lastname' => 'Mathilde',
-        'firstname' => 'pierrau',
-        'age' => 29,
-        'gender' => 'femme',
-        'mail' => 'mathilde.pierrau@gmail.com',
-        'zipcode' => '93110',
-        'picture' => '\assets\img\mathilde.jpg',
+        'lastname' => 'Charpentier',
+        'firstname' => 'Emma',
+        'age' => 36,
+        'gender' => 'Femme',
+        'zipcode' => '93786',
+        'picture' => '\assets\img\emma.jpg',
         'description' => 'jeune femme romatique ........'
     ],
     [
-        'lastname' => 'Mathilde',
-        'firstname' => 'pierrau',
-        'age' => 29,
-        'gender' => 'femme',
-        'mail' => 'mathilde.pierrau@gmail.com',
-        'zipcode' => '93110',
-        'picture' => '\assets\img\mathilde.jpg',
+        'lastname' => 'Varela',
+        'firstname' => 'Alice',
+        'age' => 42,
+        'gender' => 'Femme',
+        'zipcode' => '95000',
+        'picture' => '\assets\img\alice.jpg',
         'description' => 'jeune femme romatique ........'
+    ],
+    [
+        'lastname' => 'Anwar',
+        'firstname' => 'Gabrielle',
+        'age' => 38,
+        'gender' => 'Femme',
+        'zipcode' => '75015',
+        'picture' => '\assets\img\gabrielle.jpg',
+        'description' => 'jeune femme romatique ........'
+    ],
+    [
+        'lastname' => 'Benali',
+        'firstname' => 'Yasmine',
+        'age' => 18,
+        'gender' => 'Femme',
+        'zipcode' => '95420',
+        'picture' => '\assets\img\yasmine.jpg',
+        'description' => ' ........'
+    ],
+    [
+        'lastname' => 'Rabah',
+        'firstname' => 'Sarah',
+        'age' => 22,
+        'gender' => 'Femme',
+        'zipcode' => '89000',
+        'picture' => '\assets\img\sarah.jpg',
+        'description' => 'instagrammeuse youtubeuse qui vends du reve  ........'
+    ],
+    [
+        'lastname' => 'Lemaire ',
+        'firstname' => 'Clemence',
+        'age' => 33,
+        'gender' => 'Femme',
+        'zipcode' => '66100',
+        'picture' => '\assets\img\clemence.jpg',
+        'description' => ' ........'
+    ],
+    [
+        'lastname' => 'Lecomte',
+        'firstname' => 'luise',
+        'age' => 17,
+        'gender' => 'Femme',
+        'zipcode' => '51020',
+        'picture' => '\assets\img\luise.jpg',
+        'description' => 'a ne pas deranger  ........'
+    ],
+    [
+        'lastname' => 'Gauthier',
+        'firstname' => 'Susane',
+        'age' => 35,
+        'gender' => 'Femme',
+        'zipcode' => '32110',
+        'picture' => '\assets\img\susane.jpg',
+        'description' => 'etudiante en medicine  ........'
+    ],
+    [
+        'lastname' => 'Aliani',
+        'firstname' => 'Salima',
+        'age' => 29,
+        'gender' => 'Femme',
+        'zipcode' => '93420',
+        'picture' => '\assets\img\salima.jpg',
+        'description' => 'Makup artiste ........'
+    ],
+    [
+        'lastname' => 'Bernard',
+        'firstname' => 'Gabriel',
+        'age' => 29,
+        'gender' => 'Homme',
+        'zipcode' => '93420',
+        'picture' => '\assets\img\gabriel.jpg',
+        'description' => 'dsfghjklmù.'
+    ],
+    [
+        'lastname' => 'Moreau',
+        'firstname' => 'Adam',
+        'age' => 39,
+        'gender' => 'Homme',
+        'zipcode' => '36520',
+        'picture' => '\assets\img\adam.jpg',
+        'description' => 'ezrfzfxde.'
+    ],
+    [
+        'lastname' => 'Petit',
+        'firstname' => 'Mael',
+        'age' => 29,
+        'gender' => 'Homme',
+        'zipcode' => '75130',
+        'picture' => '\assets\img\mael.jpg',
+        'description' => 'pojkngbjhbjh'
+    ],
+    [
+        'lastname' => 'Berger',
+        'firstname' => 'Ethan',
+        'age' => 42,
+        'gender' => 'Homme',
+        'zipcode' => '93420',
+        'picture' => '\assets\img\ethan.jpg',
+        'description' => 'dsfghjklmù.'
+    ],
+    [
+        'lastname' => 'Colin',
+        'firstname' => 'Simon',
+        'age' => 19,
+        'gender' => 'Homme',
+        'zipcode' => '94020',
+        'picture' => '\assets\img\Simon.png',
+        'description' => 'dsfghjklmù.'
+    ],
+    [
+        'lastname' => 'Simard',
+        'firstname' => 'Leandre',
+        'age' => 29,
+        'gender' => 'Homme',
+        'zipcode' => '91100',
+        'picture' => '\assets\img\leandre.jpg',
+        'description' => 'plmakzasjldnjkzeh'
+    ],
+    [
+        'lastname' => 'Achour',
+        'firstname' => 'Yanis',
+        'age' => 25,
+        'gender' => 'Homme',
+        'zipcode' => '75015',
+        'picture' => '\assets\img\yanis.jpg',
+        'description' => 'aaaaaaaddddddd.'
+    ],
+    [
+        'lastname' => 'Gallard',
+        'firstname' => 'Paul',
+        'age' => 76,
+        'gender' => 'Homme',
+        'zipcode' => '95110',
+        'picture' => '\assets\img\paul.jpg',
+        'description' => 'un vieu qui refuse d\'assumer son age hahaha'
+    ],
+    [
+        'lastname' => 'Lecomte',
+        'firstname' => 'Arthur',
+        'age' => 41,
+        'gender' => 'Homme',
+        'zipcode' => '76220',
+        'picture' => '\assets\img\arthur.jpg',
+        'description' => 'oooooooooooooooooo.'
     ]
+
+
 ];
 ?>
